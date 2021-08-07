7d6247a643b6cb8dd55516b49ad51e743aa0f572
Leif Bersamina <leifbersamina@gmail.com>
2021-08-07 22:25:00 +0800

    add first component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function ExpenseItem(){
+  return ( 
+    <div>
+      <h2>Date</h2> 
+      <div>
+        <h2>Title</h2>
+        <h3>Amount</h3>
+      </div>
+    </div>
+    
+  );
+}
+
+export default ExpenseItem
