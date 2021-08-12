ecaf255fa12a16ec7daaa39d9ff3fa55ed2f1cbc
Leif Bersamina <leifbersamina@gmail.com>
2021-08-12 20:34:18 +0800

    add events for backdrop and Modal

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,26 @@
-import React from 'react';
-function ExpenseItem(){
-  return ( 
-    <div>
-      <h2>Date</h2> 
-      <div>
-        <h2>Title</h2>
-        <h3>Amount</h3>
+import React, {useState} from 'react';
+import ExpenseDate from './ExpenseDate'
+import '../styles/ExpenseItem.css'
+import Backdrop from './Backdrop';
+
+
+function ExpenseItem(props){
+  const [name, setName] = useState(props.name);
+  const [isModalOpen, setModal] = useState(false)
+  const clickHandler = () =>{ setName('Updated Car Again') }
+  const deleteHandler = () =>{ setModal(true) }
+  const closeModal = () => setModal(false)
+  
+  return (  
+      <div className = 'expense-item'>
+        <ExpenseDate date = {props.date}/>
+          <h2>{name}</h2>
+          <h3>Php {props.amount}</h3>
+          <button onClick= {clickHandler} className = 'btn update'>Update</button>
+          <button onClick= {deleteHandler} className = 'btn update'>Delete</button>
+          {isModalOpen && <Backdrop onCancel = {closeModal}/>}
+          
       </div>
-    </div>
     
   );
 }
